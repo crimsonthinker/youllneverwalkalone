2cf14696fd8e3f27b1930d3545727651131452e3
caochanhduong <1610571@hcmut.edu.vn>
2018-10-11 12:01:26 +0700

    forth commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ class Nav extends Component {
  <nav className="navbar navbar-expand navbar-dark bg-success static-top">
   <a className="navbar-brand mr-1" href="index.html">Smart Farm</a>
   <button className="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-    <i className="fas fa-bars" />
+  <img src="http://www.pngall.com/wp-content/uploads/2017/05/Save-Tree-PNG.png" alt="Smiley face" height="42" width="42"/>
   </button>
   {/* Navbar Search */}
   <form className="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
