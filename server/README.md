119164cc212a36e3c3782a0f2987a55c98ea785c
Nguyễn Phan Đăng Khoa <khoanpd@younetgroup.com>
2018-10-12 13:55:54 +0700

    Adding java streaming server

---
+++ server/README.md
@@ -0,0 +1,8 @@
+1. To compile java file:
+```
+javac -cp "./kafka/libs/*" DataProducer.java
+```
+2. To exucute java class:
+```
+java -cp ./kafka/libs/*: DataProducer
+```
--- server/_kafka_producer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-import time
-producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092')
-while(True):
-    producer.send('basic_topic',b'hello')
-    time.sleep(1)
-zk.stop()
